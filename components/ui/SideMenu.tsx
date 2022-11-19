79b06fbec67c230e692b53867bedd831384c8572
unknown <dag_98@hotmail.com>
2022-11-19 01:41:18 -0600

    add dashboard page

--- components/ui/SideMenu.tsx
+++ components/ui/SideMenu.tsx
@@ -1,6 +1,6 @@
 import { useContext, useState } from "react";
 import { Box, Divider, Drawer, IconButton, Input, InputAdornment, List, ListItem, ListItemIcon, ListItemText, ListSubheader } from "@mui/material"
-import { AccountCircleOutlined, AdminPanelSettings, CategoryOutlined, ConfirmationNumberOutlined, EscalatorWarningOutlined, FemaleOutlined, LoginOutlined, MaleOutlined, SearchOutlined, VpnKeyOutlined } from "@mui/icons-material"
+import { AccountCircleOutlined, AdminPanelSettings, CategoryOutlined, ConfirmationNumberOutlined, EscalatorWarningOutlined, FemaleOutlined, LoginOutlined, MaleOutlined, SearchOutlined, VpnKeyOutlined, DashboardOutlined } from '@mui/icons-material';
 import { UIContext, AuthContext } from "../../context";
 import { useRouter } from "next/router";
 
@@ -139,6 +139,15 @@ export const SideMenu = () => {
                                 <Divider />
                                 <ListSubheader>Admin Panel</ListSubheader>
 
+                                <ListItem 
+                                    button
+                                    onClick={() => navigate('/admin/')}
+                                >
+                                    <ListItemIcon>
+                                        <DashboardOutlined/>
+                                    </ListItemIcon>
+                                    <ListItemText primary={'Dashboard'} />
+                                </ListItem>
                                 <ListItem button>
                                     <ListItemIcon>
                                         <CategoryOutlined/>
